27ebe066410fafb402ddbcf217613d1565b00be3
aditechdev <aditechdevofficial@gmail.com>
2021-07-25 12:38:04 +0530

    :sparkles: Added App Bar

--- lib/main.dart
+++ lib/main.dart
@@ -5,3 +5,16 @@
 // 3) Split the app into three widget: App, TextControl & Text
 
 import 'package:flutter/material.dart';
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Assignment 1"),
+        ),
+      ),
+    );
+  }
+}
