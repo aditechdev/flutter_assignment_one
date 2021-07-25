2f22de728062ed1d4827589f3a9d90730b49d366
aditechdev <aditechdevofficial@gmail.com>
2021-07-25 12:36:04 +0530

    :tada: Assignment one begin

---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+// 1) Create a new Flutter app (in this project ) and output an AppBar and
+// some text below it
+
+// 2) Add a button which change the text to any other text of your choice
+// 3) Split the app into three widget: App, TextControl & Text
+
